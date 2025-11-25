9c5d7222bc86d1c868d468b95089f4fd8430c54e
phantomind <kooler2169@gmail.com>
2025-11-25 18:35:00 +0300

    Исправлены недочеты с библиотеками

--- test_add_group.py
+++ test_add_group.py
@@ -1,52 +1,41 @@
 # -*- coding: utf-8 -*-
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Chrome(executable_path=r'')
-        self.wd.implicitly_wait(30)
-    
-    def test_add_group(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("123")
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("123")
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("123")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
 
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture(scope="function")
+def driver():
+    # Можно добавить опции, если нужно (например, headless)
+    # options = webdriver.FirefoxOptions()
+    # driver = webdriver.Firefox(service=FirefoxService(), options=options)
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
 
-if __name__ == "__main__":
-    unittest.main()
+
+def test_add_user_group(driver):
+    driver.get("https://localhost/addressbook/")
+
+    # Авторизация
+    driver.find_element(By.NAME, "user").send_keys("admin")
+    driver.find_element(By.NAME, "pass").send_keys("secret")
+    driver.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    # Переход к группам
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.LINK_TEXT, "groups"))
+    ).click()
+
+    # Создание новой группы
+    driver.find_element(By.NAME, "new").click()
+    driver.find_element(By.NAME, "group_name").send_keys("123")
+    driver.find_element(By.NAME, "submit").click()
+
+    # Выход
+    driver.find_element(By.LINK_TEXT, "Logout").click()
