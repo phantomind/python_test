8f01c498b57992dd57e959636fe61ffdd4b60b76
phantomind <kooler2169@gmail.com>
2025-12-08 12:07:37 +0300

    Выделены вспомогательные методы и классы

--- test_add_group2.py
+++ test_add_group2.py
@@ -5,7 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+from group import Group
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -15,9 +16,18 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, Group(name="1234", header="1234", footer="1234"))
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -25,31 +35,31 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element(By.LINK_TEXT, "Logout").click()
 
     def return_to_groups_page(self, wd):
-        wd.find_element(By.LINK_TEXT, "groups page").click()
+        wd.find_element(By.LINK_TEXT, "group page").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, group):
         # init groups page
         wd.find_element(By.NAME, "new").click()
         # fill group firm
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys("1234")
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
         wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys("1234")
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
         wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys("1234")
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element(By.NAME, "submit").click()
 
     def open_groups_page(self, wd):
         wd.find_element(By.LINK_TEXT, "groups").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element(By.NAME, "user").click()
         wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys("admin")
+        wd.find_element(By.NAME, "user").send_keys(username)
         wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys("secret")
+        wd.find_element(By.NAME, "pass").send_keys(password)
         wd.find_element(By.XPATH, "//input[@value='Login']").click()
         wd.find_element(By.ID, "content").click()
 
