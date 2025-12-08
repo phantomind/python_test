7a8a70b4c3ebd3bf02a73b7a7d52d0395e2d8b98
phantomind <kooler2169@gmail.com>
2025-12-08 15:59:57 +0300

    Выделен класс для работы с группами

---
+++ fixture/group.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
+class GroupHelper:
+    def __init__(self, app):
+        self.app = app
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        wd.find_element(By.LINK_TEXT, "group page").click()
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init groups page
+        wd.find_element(By.NAME, "new").click()
+        # fill group firm
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element(By.NAME, "submit").click()
+        self.return_to_groups_page()
+
+    def open_groups_page(self):
+        wd = self.app.wd
+        wd.find_element(By.LINK_TEXT, "groups").click()
--- test/test_add_group2.py
+++ test/test_add_group2.py
@@ -12,10 +12,10 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="1234", header="1234", footer="1234"))
+    app.group.create(Group(name="1234", header="1234", footer="1234"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
