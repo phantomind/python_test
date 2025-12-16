fddba2f2504082041fd100dde19faff46f26fd30
phantomind <kooler2169@gmail.com>
2025-12-16 22:41:56 +0300

    Построены генераторы тестовых данных + реализован тест на заполнения номеров телефона

--- test/conftest.py
+++ test/conftest.py
@@ -1,33 +1,52 @@
 from email.policy import default
-
+import json
 import pytest
+import os.path
+import importlib
+import jsonpickle
 from fixture.application import Application
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
-    if fixture is None:
-        browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--baseUrl")
-        fixture = Application(browser=browser, base_url=base_url)
-        fixture.session.login(username="admin", password="secret")
-    else:
-        if not fixture.is_valid():
-            fixture = Application()
-    # fixture.session.ensure_login(username="admin", password="secret")
+    global target
+    browser = request.config.getoption("--browser")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        # fixture.session.ensure_logout()
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+def load_from_json(file):
+    with open (os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
--- test/test_phones.py
+++ test/test_phones.py
@@ -21,4 +21,3 @@ def merge_phones_like_on_home_page(contact):
                             map(lambda  x: clear(x),
                                 filter(lambda x: x is not None,
                                        [contact.homephone, contact.workphone, contact.mobilephone, contact.secondaryphone]))))
-
